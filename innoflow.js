bb2190dfe949e0b6a3ae406e97cc9cb46253f4ac
Jack Roper <roper.jack.h@gmail.com>
2017-03-07 22:54:24 +0000

    Initial commit

---
+++ innoflow.js
@@ -0,0 +1,74 @@
+const domain = "https://innoflow.herokuapp.com/"
+	, api = domain + "api/"
+	, searchRoute = api + "classes/admins/search"
+	, usersWebRoute = domain + "users/";
+
+document.addEventListener('DOMContentLoaded', addListeners, false);
+
+function addListeners() {
+	let input = document.getElementById("search");
+	let debouncedSearch = debounce(search, 300, false);
+	input.addEventListener("input", debouncedSearch);
+}
+
+function search(val) {
+	let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200 && this.responseType === "json") {
+			populateResults(this.response);
+		}
+  };
+  xhr.open("GET", searchRoute, true);
+  xhr.send();
+}
+
+function populateResults(results) {
+	let list = document.getElementById("results");
+	let i = 0;
+	for (; i < results.length && i < list.rows.length; i++) {
+		if (i >= list.rows.length) {
+			list.insertRow(i);			
+		}
+		setResult(list.rows[i], results[i].userId, results[i].username);
+	}
+	while (list.rows.length > i) {
+		list.deleteRow(i);
+	}
+}
+
+function setResult(row, id, username) {
+	row.setAttribute("id", id);
+	if (row.cells.length > 0) {
+		row.deleteCell(0);
+	}	
+	let td = row.insertCell(0);
+	td.innerText = username;
+	row.addEventListener("click", selectUser);	
+}
+
+function selectUser() {
+	if (this.id) {
+		let url = usersWebRoute + this.id + "/innovations";
+    chrome.tabs.create({ url });
+	}	
+}
+
+function debounce(func, wait, immediate) {
+	let timeout;
+	return function() {
+		let context = this;
+		let args = arguments;
+		let later = function() {
+			timeout = null;
+			if (!immediate) {
+				func.apply(context, args);
+			}
+		};
+		let callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) {
+			func.apply(context, args);
+		}
+	};
+};
